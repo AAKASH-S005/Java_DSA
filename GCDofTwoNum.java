85f209e055ac8a340eab13becce1b71caeb22dc4
Aakash.S <aakashselvaraj2005@gmail.com>
2025-03-27 12:24:20 +0530

    Create GCDofTwoNum.java
    Java Program to find the GCD(Greatest Common Divisor)of  two numbers using Recursion

---
+++ GCDofTwoNum.java
@@ -0,0 +1,21 @@
+Java Program to find the GCD(Greatest Common Divisor)of  two numbers using Recursion
+
+CODE :
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static int gcd(int n1,int n2){
+        if(n1 == 0)
+            return n2;
+        return gcd(n1%n2 ,n1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(gcd(a,b));
+    }
+}
